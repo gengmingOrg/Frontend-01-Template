7b8f542bf6883e090190cab4a88bd2add54528b1
gengming <1154445357@qq.com>
2020-04-21 23:22:02 +0800

    第二周作业

---
+++ week02/随堂笔记.js
@@ -0,0 +1,27 @@
+for (var i = 0; i < 128; i++) {
+  document.write("<p style='background-color: antiquewhite'>" + String.fromCharCode(i)+'</p>')
+}
+var 厉害 = 1
+console.log(厉害);
+
+/**
+  Bom ?
+  nbsp no-break space
+  codePointAt() // 转换 unicode
+  //\u115 转译
+  basic mulitiling plane bmp 基本字符平面
+  zw zero - width
+  https://unicode.org/reports/tr31/ command+f search "id_start"
+*/
+
+var a = 0.1
+var b = 0.2
+const memory = new Float64Array(1);
+memory[0] = a
+const intarr = new UnintArray(memory.buffer);
+intarr[0] = a
+
+
+// 小作业：写一个正则 匹配所有Number直接量
+
+//正则 https://tool.oschina.net/uploads/apidocs/jquery/regexp.html
